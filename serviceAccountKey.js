f0b34863fa1566efa044c04c0becb01e217dcb84
Usman <udeveloper1234@gmail.com>
2024-06-22 22:22:25 +0500

    service key updated1

--- serviceAccountKey.js
+++ serviceAccountKey.js
@@ -3,7 +3,7 @@ const serviceAccount = {
   "type": "service_account",
   "project_id": "stripe-subscription-89332",
   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   "client_id": process.env.FIREBASE_CLIENT_ID,
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
